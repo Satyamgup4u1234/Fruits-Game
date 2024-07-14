d6683db151e2aabdaf3bfbe2670013fea33ebf72
Satyam Gupta <139220995+Satyamgup4u1234@users.noreply.github.com>
2024-07-14 23:11:24 +0530

    Update script1.js

--- nextpg/script1.js
+++ nextpg/script1.js
@@ -1,13 +1,6 @@
 // Initialize or retrieve the array from local storage
 let valuesArray = JSON.parse(localStorage.getItem('valuesArray')) || [1];
 
-// Predefined arrays for fruit results
-const avocado = [1, 0, 1, 0];
-const kiwi = [1, 1, 0, 1];
-const blueberry = [1, 0, 0, 1];
-const cherry = [1, 1, 1, 0];
-const fig = [1, 0, 1, 1];
-const pomegranate = [1, 1, 1, 1];
 
 // Function to handle the "Yes" button click
 function handleYesClick() {
@@ -53,33 +46,6 @@ function navigateToNextPage() {
     window.location.href = nextPage; // Navigate to the next page
 }
 
-// Function to compare arrays for equality
-function arraysEqual(arr1, arr2) {
-    if (arr1.length !== arr2.length) return false;
-    for (let i = 0; i < arr1.length; i++) {
-        if (arr1[i] !== arr2[i]) return false;
-    }
-    return true;
-}
-
-// Function to determine result by image
-function resultByImage() {
-    if (arraysEqual(valuesArray, avocado)) {
-        console.log("avocado");
-    } else if (arraysEqual(valuesArray, kiwi)) {
-        console.log("kiwi");
-    } else if (arraysEqual(valuesArray, blueberry)) {
-        console.log("blueberry");
-    } else if (arraysEqual(valuesArray, cherry)) {
-        console.log("cherry");
-    } else if (arraysEqual(valuesArray, fig)) {
-        console.log("fig");
-    } else if (arraysEqual(valuesArray, pomegranate)) {
-        console.log("pomegranate");
-    } else {
-        console.log("Match Not Found");
-    }
-}
 
 // Add event listeners to the buttons after DOM is fully loaded
 document.addEventListener('DOMContentLoaded', function() {
